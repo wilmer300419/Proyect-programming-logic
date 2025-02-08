efd8f268cdb5e1385a84a1eedfaed72a6003571e
wilmer300419 <wilmercapera.ah@academia.umb.edu.co>
2025-02-08 09:34:38 -0500

    Create Persona.java

---
+++ taller1/tallerPoo/Persona.java
@@ -0,0 +1,48 @@
+public class Persona {
+
+    private Long codIdentificacion;
+    private String nombre;
+    private String apellido;
+
+
+    public Persona(Long codIdentificacion, String nombre, String apellido) {
+        this.codIdentificacion = codIdentificacion;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+
+    public Long getCodIdentificacion() {
+        return codIdentificacion;
+    }
+
+
+    public void setCodIdentificacion(Long codIdentificacion) {
+        this.codIdentificacion = codIdentificacion;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    
+
+    
+    
+}
