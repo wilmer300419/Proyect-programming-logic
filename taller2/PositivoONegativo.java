3b4db448519f89d9d8f49e52594506c3ad71f2c0
wilmer300419 <wilmercapera.ah@academia.umb.edu.co>
2025-02-15 20:54:31 -0500

    Create PositivoONegativo.java

---
+++ taller2/PositivoONegativo.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+
+public class PositivoONegativo{
+
+    // Variable de recoleccion de datos
+    private static Scanner readScanner = new Scanner(System.in);
+
+    // Metodo de identificación de datos positivo o negativo
+public static String identificaNumeroPositivoONegativo(Double num){
+    if(num>0){
+        return "El número ingresado es positivo.";
+    }else if (num<0) {
+        return "El número ingresado es negativo.";
+    }else if (num == 0){
+        return "El número ingresado es 0.";
+    }
+    
+    return "Ingreso un dato diferente a un número";
+}
+
+// Metodo de ejecución del proyecto
+    public static void main(String[] args) {
+        System.out.println("Ingrese un numero");
+        Double num = readScanner.nextDouble();
+        String resPositNegat = identificaNumeroPositivoONegativo(num);
+        System.out.println(resPositNegat);
+    }
+}
