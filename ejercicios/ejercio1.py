0b0cce3c55a2093f555f6f052ecaeef8affd344c
wilmer300419 <wilmercapera.ah@academia.umb.edu.co>
2025-02-17 11:29:46 -0500

    Create ejercio1.py

---
+++ ejercicios/ejercio1.py
@@ -0,0 +1,18 @@
+dia = 3
+match dia:
+    case 1:
+        print("Lunes")
+    case 2:
+        print("Martes")
+    case 3:
+        print("Miercoles")
+    case 4:
+        print("Jueves")
+    case 5:
+        print("Viernes")
+    case 6:
+        print("Sabado")
+    case 7:
+        print("Domingo")
+    case _:
+        print("Dia no existe")
