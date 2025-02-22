3d8e3849932be757139fa4a228a75dc989d70a17
wilmer300419 <wilmercapera.ah@academia.umb.edu.co>
2025-02-22 17:36:47 -0500

    Update par_o_impar.py

--- taller2_py/par_o_impar.py
+++ taller2_py/par_o_impar.py
@@ -1,10 +1,10 @@
 # Solicita al usuario que ingrese un número y lo convierte a tipo flotante
 num = float(input("Ingrese un número: "))
 
-# Verifica si el número es par o impar
-if num % 2 == 0:  # Si el residuo de la división entre 2 es 0, es un número par
-    print("El número ingresado es par.")
-elif num < 0:  # Si el el numero es negativo es un dato erroneo
+if num < 0:  # Si el el numero es negativo es un dato erroneo
     print("Ingreso un dato erróneo")
+# Verifica si el número es par o impar
+elif num % 2 == 0:  # Si el residuo de la división entre 2 es 0, es un número par
+    print("El número ingresado es par.")
 else: 
     print("El número ingresado es impar.") 
